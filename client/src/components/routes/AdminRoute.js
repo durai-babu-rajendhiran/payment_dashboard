e0547eddfa7be4e735ebaba5e88771e8c7cf8b32
durai-babu-rajendhiran <duraibabu200@gmail.com>
2025-07-01 06:41:15 +0530

    changes Added

--- client/src/components/routes/AdminRoute.js
+++ client/src/components/routes/AdminRoute.js
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import LoadingToRedirect from "./LoadingToRedirect";
-import { useNavigate } from "react-router-dom";
+import { useNavigate ,useLocation} from "react-router-dom";
 
 const AdminRoute = (Component) => {
   const { user } = useSelector((state) => ({ ...state }));
   const [ok, setOk] = useState(false);
   const navigate = useNavigate()
-
+  const location = useLocation()
 
   useEffect(() => {
    
     if (user && user.token) {
         setOk(true);
-    }else{
+    }else if(location.pathname != "/register"){
       navigate("/login");
-
     };
-  
   }, [user]);
 
   return ok ? <Component /> : <LoadingToRedirect />;
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
         type: "LOGGED_IN_USER",
         payload: userData,
       });
-      navigate("/");
+      window.location.href = "/";
       }
     } catch (error) {
       toast.error(error.message);
